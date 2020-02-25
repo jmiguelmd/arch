9714f4c75230327f309bc230790a2a147c53f1a6
jmiguelmd <jmiguelmd98@gmail.com>
2020-02-25 21:41:17 +0000

    .bashrc

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 rsync -avh --progress touchpad/30-touchpad.conf /etc/X11/xorg.conf.d/
-
+rsync -avh --progress .bashrc ~/.bashrc
--- update.sh
+++ update.sh
@@ -3,4 +3,4 @@
 rsync -avh --progress ~/scripts .
 rsync -avh --progress ~/.config/i3/* i3/
 rsync -avh --progress /etc/X11/xorg.conf.d/30-touchpad.conf touchpad/
-
+rsync -avh --progress ~/.bashrc .
