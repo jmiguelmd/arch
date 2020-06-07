2860cf787daf7f25343ae1370666e2f5b0ab69d9
João Dias <jmiguelmd98@gmail.com>
2020-06-07 17:42:23 +0100

    Updated the install script

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-rsync -avh --progress touchpad/30-touchpad.conf /etc/X11/xorg.conf.d/
-rsync -avh --progress .bashrc ~/.bashrc
+cp bash/.bashrc ~/.bashrc
+cp i3/config ~/.config/i3
+cp i3/i3blocks.conf ~/.config/i3
+cp -r scripts/* ~/scripts/
+cp tmux/.tmux.conf ~/.tmux.conf
+cp xinit/.xinitrc ~/.xinitrc
+cp touchpad/30-touchpad.conf /etc/X11/xorg.conf.d/
