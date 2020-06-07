9e74673ae176e8eb450beb7698d9005e0aca0978
João Dias <jmiguelmd98@gmail.com>
2020-06-07 18:51:33 +0100

    Another gtk config

--- install.sh
+++ install.sh
@@ -12,5 +12,6 @@ firefox_profile=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// 
 cp firefox/user.js ~/.mozilla/firefox/$firefox_profile/
 
 cp gtk/settings.ini ~/.config/gtk-3.0/
+cp gtk/.gtkrc-2.0 ~/
 cp gnome-terminal/user ~/.config/dconf/
 cp -r autokey_scripts/ ~/
