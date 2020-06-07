879ac2bcd4692791db1d2c00eec30060a0c6c1c6
João Dias <jmiguelmd98@gmail.com>
2020-06-07 17:22:31 +0100

    Updated the update script

--- update.sh
+++ update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-rsync -avh --progress ~/scripts .
-rsync -avh --progress ~/.config/i3/* i3/
-rsync -avh --progress /etc/X11/xorg.conf.d/30-touchpad.conf touchpad/
-rsync -avh --progress ~/.bashrc .
+cp -r ~/scripts .
+cp -r ~/.config/i3/* i3/
+cp /etc/X11/xorg.conf.d/30-touchpad.conf touchpad/
+cp ~/.bashrc bash/
+cp ~/.tmux.conf tmux/
