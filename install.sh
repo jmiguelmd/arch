2faba4c7aeb5b2dc8de5cae9ec24678b3b00e65b
João Dias <jmiguelmd98@gmail.com>
2020-06-09 20:02:32 +0100

    Create necessary folders

--- install.sh
+++ install.sh
@@ -1,22 +1,34 @@
 #!/bin/bash
 
-pacman -S --needed - < pacman/pkglist
+home=/home/m
 
-cp bash/.bashrc ~/.bashrc
-cp i3/config ~/.config/i3
-cp i3/i3blocks.conf ~/.config/i3
-cp -r scripts/* ~/scripts/
-cp tmux/.tmux.conf ~/.tmux.conf
-cp xinit/.xinitrc ~/.xinitrc
+pacman -S --needed $(cat pacman/pkglist | grep -v intel-ucode)
+
+# disable beep
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+
+cp bash/.bashrc $home/.bashrc
+cp tmux/.tmux.conf $home/.tmux.conf
+cp xinit/.xinitrc $home/.xinitrc
 cp touchpad/30-touchpad.conf /etc/X11/xorg.conf.d/
 
-firefox_profile=$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep release)
-cp firefox/user.js ~/.mozilla/firefox/$firefox_profile/
+mkdir -p $home/.config/i3
+cp i3/config $home/.config/i3/
+cp i3/i3blocks.conf $home/.config/i3/
 
-cp gtk/settings.ini ~/.config/gtk-3.0/
-cp gtk/.gtkrc-2.0 ~/
-cp gnome-terminal/user ~/.config/dconf/
-cp -r autokey_scripts/ ~/
+cp -r scripts/* $home/scripts/
 
-cp vim/.vimrc ~/
-cp -r vim/.vim ~/
+firefox_profile=$(grep 'Path=' $home/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep release)
+cp firefox/user.js $home/.mozilla/firefox/$firefox_profile/
+
+mkdir -p $home/.config/gtk-3.0
+cp gtk/settings.ini $home/.config/gtk-3.0/
+cp gtk/.gtkrc-2.0 $home/
+
+mkdir -p $home/.config/dconf/
+cp gnome-terminal/user $home/.config/dconf/
+
+cp -r autokey_scripts/ $home/
+
+cp vim/.vimrc $home/
+cp -r vim/.vim $home/
